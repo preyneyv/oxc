2de9ef578ecac4d9c99cfca171a0dbdbb4d9a66c
Ottomated <otto@ottomated.net>
2024-10-23 11:06:20 -0700

    commonjs

--- napi/parser/index.js
+++ napi/parser/index.js
@@ -1,14 +1,15 @@
-import * as bindings from './bindings.cjs';
-export * from './bindings.cjs';
+const bindings = require('./bindings.js');
 
-export function parseSync(...args) {
-  const result = bindings.parseSync(...args);
-  result.program = JSON.parse(result.program);
-  return result;
-}
-
-export async function parseAsync(...args) {
-  const result = await bindings.parseAsync(...args);
-  result.program = JSON.parse(result.program);
-  return result;
-}
+module.exports = {
+  ...bindings,
+  parseSync(...args) {
+    const result = bindings.parseSync(...args);
+    result.program = JSON.parse(result.program);
+    return result;
+  },
+  async parseAsync(...args) {
+    const result = await bindings.parseAsync(...args);
+    result.program = JSON.parse(result.program);
+    return result;
+  },
+};
