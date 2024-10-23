c2847d3109f7fb25f19b32e87cee8b81f2780e41
Ottomated <otto@ottomated.net>
2024-10-23 11:15:23 -0700

    export manually

--- napi/parser/index.js
+++ napi/parser/index.js
@@ -1,15 +1,16 @@
-const bindings = require('./bindings.js');
+const bindings = require("./bindings.js");
 
-module.exports = {
-  ...bindings,
-  parseSync(...args) {
-    const result = bindings.parseSync(...args);
-    result.program = JSON.parse(result.program);
-    return result;
-  },
-  async parseAsync(...args) {
-    const result = await bindings.parseAsync(...args);
-    result.program = JSON.parse(result.program);
-    return result;
-  },
+module.exports.moduleLexerAsync = bindings.moduleLexerAsync;
+module.exports.moduleLexerSync = bindings.moduleLexerSync;
+module.exports.parseWithoutReturn = bindings.parseWithoutReturn;
+
+module.exports.parseAsync = async function parseAsync(...args) {
+  const result = await bindings.parseAsync(...args);
+  result.program = JSON.parse(result.program);
+  return result;
+};
+module.exports.parseSync = function parseSync(...args) {
+  const result = bindings.parseSync(...args);
+  result.program = JSON.parse(result.program);
+  return result;
 };
